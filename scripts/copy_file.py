67d9ed9f198c3a777dbd1495c48c7e4f93663558
Ranganath <maitreyr@f45c8989d143.ant.amazon.com>
2019-08-08 19:09:24 -0500

    Cleaned up templates

--- scripts/copy_file.py
+++ scripts/copy_file.py
@@ -25,12 +25,8 @@ def get_private_key():
             SecretId=secret_name
         )
     except ClientError as e:
-        if e.response['Error']['Code'] == 'ResourceNotFoundException':
-            print("The requested secret " + secret_name + " was not found")
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            print("The request was invalid due to:", e)
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            print("The request had invalid params:", e)
+        raise e
+
     else:
         secret = get_secret_value_response['SecretString']
             
